5f69aff94625852b35ffc59aedc6c1e8687d3420
tll549 <tll549@yahoo.com.tw>
2019-06-11 17:20:00 -0500

    src/components/scatter1_selectYAxis.js

--- src/components/scatter1_selectYAxis.js
+++ src/components/scatter1_selectYAxis.js
@@ -44,7 +44,7 @@ export default class Scatter1_selectYAxis extends Component {
           <MarkSeries
             className="scatter1"
             data={reformatedData}/>
-          <XAxis/>
+          <XAxis tickFormat={v => v} tickValues={[5,10,20,30,40,50,100,200,300,400]}/>
           <YAxis/>
           <ChartLabel
             text={title}
@@ -57,7 +57,7 @@ export default class Scatter1_selectYAxis extends Component {
               fontWeight: 10000
             }}/>
           <ChartLabel
-            text="Price"
+            text="Price ($)"
             className="alt-x-label"
             includeMargin={false}
             xPercent={0.45}
