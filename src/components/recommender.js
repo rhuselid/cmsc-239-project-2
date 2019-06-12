3d4b1630659b334d9ce5c8eef95a1a0a2d9bd62c
Genevieve Faber <genevievefaber@Genevieves-MacBook-Pro-2.local>
2019-06-12 17:28:59 -0500

    fixed bug in tooltip

--- src/components/recommender.js
+++ src/components/recommender.js
@@ -77,7 +77,7 @@ export default class Recommender extends Component {
       size={5}
       xDomain={[this.state.value.min > 5 ? this.state.value.min - 5 : 0, this.state.value.max + 5]}
       yDomain={[80, 100]}
-      onMouseLeave={() => this.setState({tooltipVal: null})}>
+      onMouseLeave={e => e.relatedTarget.classList.contains("winehint") ? null : this.setState({tooltipVal: null})}>
       <MarkSeries
         className="topWines"
         data={topWines}
